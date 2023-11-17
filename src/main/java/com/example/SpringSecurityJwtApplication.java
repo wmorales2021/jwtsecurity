841117ee33e6195ef4ea20b8dc29c8136f6bbeac
unknown <wmorales@fna.gov.co>
2023-11-17 18:48:23 -0500

    jwtsecurity1

---
+++ src/main/java/com/example/SpringSecurityJwtApplication.java
@@ -0,0 +1,66 @@
+package com.example;
+
+import com.example.models.ERole;
+import com.example.models.RoleEntity;
+import com.example.models.UserEntity;
+import com.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Bean
+	CommandLineRunner init(){
+		return args -> {
+
+			UserEntity userEntity = UserEntity.builder()
+					.email("nada@mail.com")
+					.username("carlos")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.ADMIN.name()))
+							.build()))
+					.build();
+
+			UserEntity userEntity2 = UserEntity.builder()
+					.email("otro@mail.com")
+					.username("luis")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.USER.name()))
+							.build()))
+					.build();
+
+			UserEntity userEntity3 = UserEntity.builder()
+					.email("pedro@mail.com")
+					.username("pedro")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.INVITED.name()))
+							.build()))
+					.build();
+
+			userRepository.save(userEntity);
+			userRepository.save(userEntity2);
+			userRepository.save(userEntity3);
+		};
+	}
+
+}
